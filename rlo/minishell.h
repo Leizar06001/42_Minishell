b25b8f500fa6606407a89922a2979e816354ee0f
Raphael Loussignian <rloussig@c2r5p1.42perpignan.fr>
2023-06-12 19:05:33 +0200

    erge

--- rlo/minishell.h
+++ rlo/minishell.h
@@ -6,29 +6,45 @@
 /*   By: rloussig <rloussig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:03:55 by mabdali           #+#    #+#             */
-/*   Updated: 2023/06/12 18:40:59 by rloussig         ###   ########.fr       */
+/*   Updated: 2023/06/12 18:59:28 by rloussig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef MINISHELL_H
+#ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <unistd.h>
 
-#include <dirent.h>
+
+typedef struct	s_fnc_path
+{
+	char	cd[255];
+	char	echo[255];
+	char	pwd[255];
+}				t_fnc_path;
 
 //STRUCTURE
-
+typedef struct s_data
+{
+	t_fnc_path	*paths;
+	char		*user;
+	char		*path;
+	char		*minishell_name;
+	char		*line;
+	char		path_fnc[10][255];
+}		t_data;
 
 //FT_STRJOIN
-size_t  ft_strlen(const char *s);
-void    *ft_memcpy(void *dest, const void *src, size_t n);
-char    *ft_strjoin(char const *s1, char const *s2);
-char	**ft_split(char const *s, char c);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
 
-# endif
+#endif
--- rlo/srcs/main.c
+++ rlo/srcs/main.c
@@ -6,11 +6,13 @@
 /*   By: rloussig <rloussig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:38:03 by mabdali           #+#    #+#             */
-/*   Updated: 2023/06/12 18:36:15 by rloussig         ###   ########.fr       */
+/*   Updated: 2023/06/12 19:05:09 by rloussig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+t_data  *datas;
 
 size_t  ft_strlen(const char *s)
 {
@@ -56,35 +58,55 @@ char    *ft_strjoin(char const *s1, char const *s2)
     return (buffer);
 }
 
+int find_fnc_path(char *fnc_name, char **path_lst, int index)
+{
+    int     i;
+    char    *tmp;
+
+    (void)index;
+    i = 0;
+    while (path_lst[i])
+    {
+        tmp = ft_strjoin(path_lst[i], fnc_name);
+        if (access(tmp, F_OK) == 0)
+        {
+            printf("%s : %s\n", fnc_name, tmp);
+        }
+        i++;
+    }
+    return (0);
+}
+
 void    find_sys_functions(void)
 {
     char    *path;
     char    **path_lst;
-    int     i;
 
     path = getenv("PATH");
     path_lst = ft_split(path, ':');
-    i = 0;
-    while (path_lst[i])
-    {
-        printf("%s\n", path_lst[i++]);
-    }
+    find_fnc_path("/chdir", path_lst, 0);
+    find_fnc_path("/echo", path_lst, 1);
+    find_fnc_path("/pwd", path_lst, 2);
+    find_fnc_path("/export", path_lst, 3);
+    find_fnc_path("/unset", path_lst, 4);
+    find_fnc_path("/env", path_lst, 5);
+    find_fnc_path("/exit", path_lst, 6);
 }
 
 int main()
 {
-	char *minishell_name;
-	char * user;
-	char *result;
+	// char *minishell_name;
+	// char * user;
+	// char *result;
 	
     find_sys_functions();
 
-	user = getenv("USER");
-	minishell_name = ft_strjoin(user, "@minishell : ");
-    while (1){
-    	result = readline(minishell_name);
-    	add_history (result);
+	// user = getenv("USER");
+	// minishell_name = ft_strjoin(user, "@minishell : ");
+    // while (1){
+    // 	result = readline(minishell_name);
+    // 	add_history (result);
 
-    	printf("%s\n", result);
-    }
+    // 	printf("%s\n", result);
+    // }
 }
