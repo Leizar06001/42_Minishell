1b372484bc7ec06af9f0ae03343dbe21afadfbac
Mohamed-Lamine Abdali <mabdali@c2r5p3.42perpignan.fr>
2023-06-12 18:18:42 +0200

    .

--- mabda/minishell.h
+++ mabda/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mabdali <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:03:55 by mabdali           #+#    #+#             */
-/*   Updated: 2023/06/12 18:05:54 by mabdali          ###   ########.fr       */
+/*   Updated: 2023/06/12 18:09:06 by mabdali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@
 #include <stdlib.h>
 
 //STRUCTURE
-
-typedef s_data
+typedef struct s_data
 {
-	char*;
+	char *user;
+	char *minishell_name;
+	char *line;
 }		t_data;
+
 //FT_STRJOIN
 size_t  ft_strlen(const char *s);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
--- mabda/srcs/main.c
+++ mabda/srcs/main.c
@@ -6,75 +6,67 @@
 /*   By: mabdali <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:38:03 by mabdali           #+#    #+#             */
-/*   Updated: 2023/06/12 18:05:56 by mabdali          ###   ########.fr       */
+/*   Updated: 2023/06/12 18:18:28 by mabdali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "../minishell.h"
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while (s[i] != '\0')
-        i++;
-    return (i);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
-void    *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t      i;
-    char        *p1;
-    char        *p2;
+	size_t	i;
+	char	*p1;
+	char	*p2;
 
-    i = 0;
-    p1 = (char *)dest;
-    p2 = (char *)src;
-    if (p1 == NULL && p2 == NULL)
-        return (NULL);
-    while (i < n)
-    {
-        p1[i] = p2[i];
-        i++;
-    }
-    return (dest);
+	i = 0;
+	p1 = (char *)dest;
+	p2 = (char *)src;
+	if (p1 == NULL && p2 == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		p1[i] = p2[i];
+		i++;
+	}
+	return (dest);
 }
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char    *buffer;
+	char	*buffer;
 
-    if (!s1 || !s2)
-        return (NULL);
-    buffer = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
-    if (!buffer)
-        return (NULL);
-    ft_memcpy(buffer, s1, ft_strlen(s1));
-    ft_memcpy(buffer + ft_strlen(s1), s2, ft_strlen(s2));
-    buffer[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-    return (buffer);
+	if (!s1 || !s2)
+		return (NULL);
+	buffer = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (!buffer)
+		return (NULL);
+	ft_memcpy(buffer, s1, ft_strlen(s1));
+	ft_memcpy(buffer + ft_strlen(s1), s2, ft_strlen(s2));
+	buffer[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (buffer);
 }
 
 void	init_struct(t_data *data)
 {
-	
+	data->user = getenv("USER");
+	data->minishell_name = ft_strjoin(data->user, "@minishell : ");
 }
 
-int main()
+int	main(void)
 {
-	char *minishell_name;
-	char * user;
-	char *result;
-	
-	user = getenv("USER");
-	minishell_name = ft_strjoin(user, "@minishell : ");
-	result = readline(minishell_name);
-	add_history (result);
+	t_data	data;
 
-	printf("%s", result);
+	init_struct(&data);
+	data.line = readline(data.minishell_name);
+	printf("%s", data.line);
 }
