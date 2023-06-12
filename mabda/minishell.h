6644929f2445f6d330cdd5011dc61cf07b937bc9
Mohamed-Lamine Abdali <mabdali@c2r5p3.42perpignan.fr>
2023-06-12 18:06:12 +0200

    .

---
+++ mabda/minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabdali <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 18:03:55 by mabdali           #+#    #+#             */
+/*   Updated: 2023/06/12 18:05:54 by mabdali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//STRUCTURE
+
+typedef s_data
+{
+	char*;
+}		t_data;
+//FT_STRJOIN
+size_t  ft_strlen(const char *s);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
+char    *ft_strjoin(char const *s1, char const *s2);
+
+
+# endif
