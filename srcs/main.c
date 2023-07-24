646239e45f6ceeeccdedaa69affbdc12972741a8
Leizar06001 <rloussignian@icloud.com>
2023-07-24 18:55:51 +0200

    add env array to execve

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: raphaelloussignian <raphaelloussignian@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:38:03 by mabdali           #+#    #+#             */
-/*   Updated: 2023/07/24 18:22:06 by raphaellous      ###   ########.fr       */
+/*   Updated: 2023/07/24 18:55:50 by raphaellous      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,7 @@
 
 t_data data;
 
-// int	exec_cmd(int id_cmd)
-// {
-// 	char	**cmd_line;
-	
-// 	cmd_line = ft_split_spaces(data.cmd[id_cmd]);
-// 	cmd_line = replace_dollar_args(cmd_line);
-// 	prt_args(cmd_line);
-// 	if (cmd_line[0] == NULL)
-// 		return (0);
-// 	if (ft_builtin_fnc(cmd_line))
-// 	{
-// 		select_output(cmd_line);
-// 		return (0);
-// 	}
-// 	else if (!ft_strcmp(cmd_line[0], "exit"))
-// 		return (1);
-// 	else
-// 	{
-// 		if (ft_execve(cmd_line))
-// 			printf("shell: command not found: %s\n", cmd_line[0]);
-// 	}
-// 	select_output(cmd_line);
-// 	return (0);
-// }
+
 
 int	main(int i, char *argv[], char **env)
 {	
@@ -73,7 +50,6 @@ int	main(int i, char *argv[], char **env)
 }
 
 /* ******* TO DO **********
-PIPPEX
 FREEEEEEEs
 
 < in.txt grep a | wc -l > out.txt
